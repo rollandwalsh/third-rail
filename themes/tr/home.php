53b0624bdfd704dfb640d5ce9c906db660ecca3c
Rolland Walsh <rollandwalsh@gmail.com>
2018-03-29 13:52:40 -0700

    adds wp code for content

--- themes/tr/home.php
+++ themes/tr/home.php
@@ -1,9 +1,22 @@
 <?php /* Template Name: Home */ get_header(); ?>
 
-
 <div class="row">
-	<div class="large-12 columns">
-		<?php the_content(); ?>
+	<div class="small-12 columns" role="main">
+
+	<?php do_action('foundationPress_before_content'); ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+			<?php do_action('foundationPress_page_before_entry_content'); ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+	<?php endwhile;?>
+
+	<?php do_action('foundationPress_after_content'); ?>
+
 	</div>
 </div>
 
