b890f7f842b36c319b35f2a7c4789a1067f0e935
Robert Falconer <robert@clankeith.com>
2018-07-24 16:48:50 -0700

    Updated homepage feature link and alt text for The Music Man

--- themes/tr/header.php
+++ themes/tr/header.php
@@ -79,7 +79,7 @@
 
       		<header id="pageHeader">
 	      		<?php if ( is_front_page() ) {?>
-	      			<a href="/announcing-our-upcoming-season/" style="display: block"><img src="<?php echo $largehero; ?>" alt="2017/2018 Season" id="hero"></a>
+	      			<a href="/company-history/coming-soon/the-music-man/" style="display: block"><img src="<?php echo $largehero; ?>" alt="The Music Man" id="hero"></a>
 	      		<?php } else { ?>
 	      			<img src="<?php echo $largehero; ?>" alt="<?php wp_title(); ?> Hero" id="hero">
 	      		<?php } ?>
